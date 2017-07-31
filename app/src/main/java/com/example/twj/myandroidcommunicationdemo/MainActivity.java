2eb228cc2242734fc2b880679a4421e5ad547460
SmlieJ <65716420@qq.com>
2017-07-31 14:44:24 +0800

    按钮跳转

--- app/src/main/java/com/example/twj/myandroidcommunicationdemo/MainActivity.java
+++ app/src/main/java/com/example/twj/myandroidcommunicationdemo/MainActivity.java
@@ -32,6 +32,60 @@ public class MainActivity extends AppCompatActivity {
                 //startActivity(i);
             }
         });
+        btn2=(Button) findViewById(R.id.btnGet);
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                //Intent是一种运行时绑定（run-time binding）机制，它能在程序运行过程中连接两个不同的组件。
+
+                //page1为先前已添加的类，并已在AndroidManifest.xml内添加活动事件(<activity android:name="page1"></activity>),在存放资源代码的文件夹下下，
+                //Intent i = new Intent();
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+                // i.setAction(Intent.ACTION_MAIN);
+                //i.addCategory(Intent.CATEGORY_HOME);
+                ////启动
+                //startActivity(i);
+            }
+        });
+        btn3=(Button) findViewById(R.id.btnPost);
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                //Intent是一种运行时绑定（run-time binding）机制，它能在程序运行过程中连接两个不同的组件。
+
+                //page1为先前已添加的类，并已在AndroidManifest.xml内添加活动事件(<activity android:name="page1"></activity>),在存放资源代码的文件夹下下，
+                //Intent i = new Intent();
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+                // i.setAction(Intent.ACTION_MAIN);
+                //i.addCategory(Intent.CATEGORY_HOME);
+                ////启动
+                //startActivity(i);
+            }
+        });
+        btn4=(Button) findViewById(R.id.btnSocket);
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                //Intent是一种运行时绑定（run-time binding）机制，它能在程序运行过程中连接两个不同的组件。
+
+                //page1为先前已添加的类，并已在AndroidManifest.xml内添加活动事件(<activity android:name="page1"></activity>),在存放资源代码的文件夹下下，
+                //Intent i = new Intent();
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+                // i.setAction(Intent.ACTION_MAIN);
+                //i.addCategory(Intent.CATEGORY_HOME);
+                ////启动
+                //startActivity(i);
+            }
+        });
     }
     private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
 }
