925ae3a1f6c2ac5796b61f36912f04a769ec43c6
SmlieJ <65716420@qq.com>
2017-11-26 12:12:56 +0800

    Login

--- app/src/main/java/com/example/twj/myandroidcommunicationdemo/Main2Activity.java
+++ app/src/main/java/com/example/twj/myandroidcommunicationdemo/Main2Activity.java
@@ -1,10 +1,17 @@
 package com.example.twj.myandroidcommunicationdemo;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -32,26 +39,70 @@ public class Main2Activity extends AppCompatActivity {
                        // GetPostUtil get = new GetPostUtil();
                        // show.setText(get.sendPost());
                        // handler.sendEmptyMessage(0x123);
+                        Message msg= new Message();
+                        msg.what=0x123;
 
                          Token.Token ken=new Token.Token();
                          String Name=TextName.getText().toString().toUpperCase();
                         String Password=ken.encodeg(TextPassword.getText().toString());
                         Model.Token ee=  ken.GetSignToken(Name.toString(),Password.toString());
+                        String megg=ee.getData().getSignToken().toString();
+                        MeggShow=megg.toString();
+                        Looper.prepare();
+                       /* myhandler.handleMessage(msg);
+                        Looper.loop();*/
+
                         HashMap<String, String> params = new HashMap<String, String>();
                         params.put("id", "1");
                         params.put("name", "wahaha");
                         HashMap<String,String> parames= ken.GetQueryString(params);
                         Object Obj=ServerGetPostUtil.sendGet("http://7a4c3864.ngrok.io/api/product/getproduct",parames.keySet().toString()
                                 ,parames.values().toString(),Name,Password.toString(),true);
+                        MeggShow=MeggShow+"Get:"+Obj.toString()+"\n\r";
+                       /* Looper.prepare();
+                        myhandler.handleMessage(msg);
+                        Looper.loop();*/
+
                         String Json="{\"id\":1,\"Name\":\"安慕希\",\"Cont\":10,\"Price\":58.5}";
                         Object Obj2=ServerGetPostUtil.sendPost("http://7a4c3864.ngrok.io/api/product/addProduct",Json,
                                 Name ,Password.toString());
+                        MeggShow=MeggShow+"Post:"+Obj2.toString()+"\n\r";
+                       /* Looper.prepare();*/
+                        myhandler.handleMessage(msg);
+                        Looper.loop();
                     }
                 }.start();
             }
         });
     }
 
+    final Handler myhandler = new Handler()  {
+        @Override
+        public void handleMessage(Message msg) {
+           if(msg.what==0x123)
+           {
+               Toast.makeText(Main2Activity.this,MeggShow.toString(),Toast.LENGTH_LONG).show();
+           }
+                //Toast.makeText(this,"clickme",Toast.LENGTH_LONG).show();
+             /*   AlertDialog.Builder alertdialogbuilder = new AlertDialog.Builder(Main2Activity.this).
+                        setTitle("系统提示").setMessage(MeggShow.toString());
+                setPositiveButton(alertdialogbuilder).
+                        create().
+                        show();*/
+        }        ;
+    };
+
+
+    private AlertDialog.Builder setPositiveButton(AlertDialog.Builder builder)
+    {
+        return builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+    }
+    private  String MeggShow;
     private  TextView TextName;
     private TextView TextPassword;
     private Button btnPost;
